e280b101498cd6536916d6a020554c51e6476508
Ralf <hartmann@nextbike.net>
2018-03-30 10:44:27 +0200

    reformat code, use stdint.h and use int8_t/uint8_t

--- des.h
+++ des.h
@@ -1,17 +1,22 @@
 #ifndef _DES_H_
-#define _DES_H_
+#define _DES_H_ _DES_H_
 
-#define ENCRYPTION_MODE 1
-#define DECRYPTION_MODE 0
+#include <stdint.h>
+
+enum {
+	DECRYPTION_MODE = 0,
+	ENCRYPTION_MODE = 1
+};
 
 typedef struct {
-	unsigned char k[8];
-	unsigned char c[4];
-	unsigned char d[4];
+	uint8_t k[8];
+	uint8_t c[4];
+	uint8_t d[4];
 } key_set;
 
-void generate_key(unsigned char* key);
-void generate_sub_keys(unsigned char* main_key, key_set* key_sets);
-void process_message(unsigned char* message_piece, unsigned char* processed_piece, key_set* key_sets, int mode);
+void generate_key(uint8_t *key);
+void generate_sub_keys(uint8_t *main_key, key_set key_sets[17]);
+void process_message(uint8_t *message_piece, uint8_t *processed_piece,
+		key_set key_sets[17], uint8_t mode);
 
-#endif
+#endif /* _DES_H_ */
